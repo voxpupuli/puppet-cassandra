7324c8e6c976b810a169411a949e7789df036fb0
dallinb <b.dalling@locp.co.uk>
2015-12-16 07:44:53 +0000

    Reincorporate idempotency test.

--- spec/acceptance/cassandra_spec.rb
+++ spec/acceptance/cassandra_spec.rb
@@ -154,32 +154,30 @@ describe 'cassandra class' do
     it { is_expected.to be_enabled }
   end
 
-# Release 1.9.2 will be making changes so these checks are currently redundant.
-#
-#  check_against_previous_version_pp = <<-EOS
-#    include cassandra
-#  EOS
-#  
-#  describe 'Ensure config file does get updated unnecessarily.' do
-#    it 'Initial install manifest again' do
-#      apply_manifest(check_against_previous_version_pp,
-#        :catch_failures => true)
-#    end
-#    it 'Copy the current module to the side without error.' do
-#      shell("cp -R /etc/puppet/modules/cassandra /var/tmp",
-#        :acceptable_exit_codes => 0)
-#    end
-#    it 'Remove the current module without error.' do
-#      shell("puppet module uninstall locp-cassandra",
-#        :acceptable_exit_codes => 0)
-#    end
-#    it 'Install the latest module from the forge.' do
-#      shell("puppet module install locp-cassandra",
-#        :acceptable_exit_codes => 0)
-#    end
-#    it 'Check install works without changes with previous module version.' do
-#      expect(apply_manifest(check_against_previous_version_pp,
-#        :catch_failures => true).exit_code).to be_zero
-#    end
-#  end
+  check_against_previous_version_pp = <<-EOS
+    include cassandra
+  EOS
+  
+  describe 'Ensure config file does get updated unnecessarily.' do
+    it 'Initial install manifest again' do
+      apply_manifest(check_against_previous_version_pp,
+        :catch_failures => true)
+    end
+    it 'Copy the current module to the side without error.' do
+      shell("cp -R /etc/puppet/modules/cassandra /var/tmp",
+        :acceptable_exit_codes => 0)
+    end
+    it 'Remove the current module without error.' do
+      shell("puppet module uninstall locp-cassandra",
+        :acceptable_exit_codes => 0)
+    end
+    it 'Install the latest module from the forge.' do
+      shell("puppet module install locp-cassandra",
+        :acceptable_exit_codes => 0)
+    end
+    it 'Check install works without changes with previous module version.' do
+      expect(apply_manifest(check_against_previous_version_pp,
+        :catch_failures => true).exit_code).to be_zero
+    end
+  end
 end
