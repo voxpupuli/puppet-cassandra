828f257cf3f9b5274fac318015817389300d1c54
Ben Dalling <b.dalling@locp.co.uk>
2015-12-29 21:01:56 +0000

    Added SimpleCov.minimum_coverage (#148).

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'rspec-puppet'
 require 'rspec-puppet-utils'
 require 'puppetlabs_spec_helper/module_spec_helper'
+require 'simplecov'
 require 'coveralls'
 
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
@@ -10,5 +11,8 @@ RSpec.configure do |c|
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
 end
+
+SimpleCov.minimum_coverage 100.0
+
 Coveralls.wear!
 at_exit { RSpec::Puppet::Coverage.report! }
