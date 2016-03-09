3e1575ce176d37bd674931e6d3b3b832ba5eed2e
Ben Dalling <b.dalling@locp.co.uk>
2016-03-09 05:58:38 +0000

    Missing files for opscenter => service_systemd (#184).

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,7 +6,9 @@
 class cassandra::params {
   if $::osfamily == 'Debian' {
     $systemctl = '/bin/systemctl'
+    $systemd_path = '/lib/systemd/system'
   } elsif $::osfamily == 'RedHat' {
     $systemctl = '/usr/bin/systemctl'
+    $systemd_path = '/usr/lib/systemd/system'
   }
 }
