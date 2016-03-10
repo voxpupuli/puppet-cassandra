41e1bc6f4c8a0144e718f84b610e152dddff1c18
Ben Dalling <b.dalling@locp.co.uk>
2016-03-10 20:21:11 +0000

    Added acceptance tests for create and drop keyspace (#92).

--- spec/acceptance/cassandra_spec.rb
+++ spec/acceptance/cassandra_spec.rb
@@ -274,6 +274,128 @@ describe 'cassandra class' do
     it { is_expected.to be_enabled }
   end
 
+  schema_testing_create_pp = <<-EOS
+    if $::osfamily == 'RedHat' and $::operatingsystemmajrelease == 7 {
+        $service_systemd = true
+    } elsif $::operatingsystem == 'Debian' and $::operatingsystemmajrelease == 8 {
+        $service_systemd = true
+    } else {
+        $service_systemd = false
+    }
+
+    if $::osfamily == 'RedHat' {
+        $cassandra_optutils_package = 'cassandra22-tools'
+        $cassandra_package = 'cassandra22'
+        $version = '2.2.5-1'
+    } else {
+        $cassandra_optutils_package = 'cassandra-tools'
+        $cassandra_package = 'cassandra'
+        $version = '2.2.5'
+    }
+
+    class { 'cassandra':
+      cassandra_9822              => true,
+      commitlog_directory_mode    => '0770',
+      data_file_directories_mode  => '0770',
+      listen_interface            => 'lo',
+      package_ensure              => $version,
+      package_name                => $cassandra_package,
+      rpc_interface               => 'lo',
+      saved_caches_directory_mode => '0770',
+      service_systemd             => $service_systemd
+    }
+
+    $simple_strategy_map = {
+      keyspace_class     => 'SimpleStrategy',
+      replication_factor => 3
+    }
+
+    $network_topology_strategy = {
+      keyspace_class => 'NetworkTopologyStrategy',
+      dc1            => 3,
+      dc2            => 2
+    }
+
+    $keyspaces = {
+      'Excelsior' => {
+        ensure          => present,
+        replication_map => $simple_strategy_map,
+        durable_writes  => false
+      },
+      'Excalibur' => {
+        ensure          => present,
+        replication_map => $network_topology_strategy,
+        durable_writes  => true
+      }
+    }
+
+    class { 'cassandra::schema':
+      keyspaces => $keyspaces
+    }
+  EOS
+
+  describe '########### Schema create.' do
+    it 'should work with no errors' do
+      apply_manifest(schema_testing_create_pp, catch_failures: true)
+    end
+    it 'check code is idempotent' do
+      expect(apply_manifest(schema_testing_create_pp,
+                            catch_failures: true).exit_code).to be_zero
+    end
+  end
+
+  schema_testing_drop_pp = <<-EOS
+    if $::osfamily == 'RedHat' and $::operatingsystemmajrelease == 7 {
+        $service_systemd = true
+    } elsif $::operatingsystem == 'Debian' and $::operatingsystemmajrelease == 8 {
+        $service_systemd = true
+    } else {
+        $service_systemd = false
+    }
+
+    if $::osfamily == 'RedHat' {
+        $cassandra_optutils_package = 'cassandra22-tools'
+        $cassandra_package = 'cassandra22'
+        $version = '2.2.5-1'
+    } else {
+        $cassandra_optutils_package = 'cassandra-tools'
+        $cassandra_package = 'cassandra'
+        $version = '2.2.5'
+    }
+
+    class { 'cassandra':
+      cassandra_9822              => true,
+      commitlog_directory_mode    => '0770',
+      data_file_directories_mode  => '0770',
+      listen_interface            => 'lo',
+      package_ensure              => $version,
+      package_name                => $cassandra_package,
+      rpc_interface               => 'lo',
+      saved_caches_directory_mode => '0770',
+      service_systemd             => $service_systemd
+    }
+
+    $keyspaces = {
+      'Excelsior' => {
+        ensure => absent,
+      }
+    }
+
+    class { 'cassandra::schema':
+      keyspaces => $keyspaces
+    }
+  EOS
+
+  describe '########### Schema drop.' do
+    it 'should work with no errors' do
+      apply_manifest(schema_testing_drop_pp, catch_failures: true)
+    end
+    it 'check code is idempotent' do
+      expect(apply_manifest(schema_testing_drop_pp,
+                            catch_failures: true).exit_code).to be_zero
+    end
+  end
+
   ###########################################################################
   # Disabling this code for now as seem to be hitting a Red Hat affecting
   # equivalent of CASSANDRA-10525 and on Debian, the cassandra::java class
