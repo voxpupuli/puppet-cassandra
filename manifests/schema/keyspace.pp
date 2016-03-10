83f48de918bdc3c1194741aa133510887a8218c0
Ben Dalling <b.dalling@locp.co.uk>
2016-03-10 18:11:24 +0000

    CREATE and DROP a keyspace (#92).

---
+++ manifests/schema/keyspace.pp
@@ -0,0 +1,54 @@
+# cassandra::schema::keyspace
+define cassandra::schema::keyspace(
+  $replication_map,
+  $ensure         = present,
+  $durable_writes = true,
+  $keyspace_name  = $title
+  ) {
+  include 'cassandra::schema'
+
+  $read_script = "DESC KEYSPACE ${keyspace_name}"
+  $read_command = "${::cassandra::schema::cqlsh_opts} -e \"${read_script}\" ${::cassandra::schema::cqlsh_conn}"
+
+  if $ensure == present {
+    $keyspace_class = $replication_map[keyspace_class]
+
+    case $keyspace_class {
+      'SimpleStrategy': {
+        $replication_factor = $replication_map[replication_factor]
+        $map_str = "{ 'class' : 'SimpleStrategy', 'replication_factor' : ${replication_factor} }"
+      }
+      'NetworkTopologyStrategy': {
+        $map_str1 = '{ \'class\' : \'NetworkTopologyStrategy\''
+        $new_map = prefix(delete($replication_map, 'keyspace_class'), '\'')
+        $map_str2 = join(join_keys_to_values($new_map, '\': '), ', ')
+        $map_str = "${map_str1}, ${map_str2} }"
+      }
+      default: {
+        $msg_part1 = "Invalid or no class (${keyspace_class}) specified for"
+        $msg_part2 = "keyspace ${keyspace_name}."
+        fail("${msg_part1} ${msg_part2}")
+      }
+    }
+
+    $create_script1 = "CREATE KEYSPACE IF NOT EXISTS ${keyspace_name}"
+    $create_script2 = "WITH REPLICATION = ${map_str}"
+    $create_script3 = "AND DURABLE_WRITES = ${durable_writes}"
+    $create_script = "${create_script1} ${create_script2} ${create_script3}"
+    $create_command = "${::cassandra::schema::cqlsh_opts} -e \"${create_script}\" ${::cassandra::schema::cqlsh_conn}"
+
+    exec { $create_command:
+      unless  => $read_command,
+      require => Exec['::cassandra::schema connection test']
+    }
+  } elsif $ensure == absent {
+    $delete_script = "DROP KEYSPACE ${keyspace_name}"
+    $delete_command = "${::cassandra::schema::cqlsh_opts} -e \"${delete_script}\" ${::cassandra::schema::cqlsh_conn}"
+    exec { $delete_command:
+      onlyif  => $read_command,
+      require => Exec['::cassandra::schema connection test']
+    }
+  } else {
+    fail("Unknown action (${ensure}) for ensure attribute.")
+  }
+}
--- spec/classes/schema_spec.rb
+++ spec/classes/schema_spec.rb
@@ -11,37 +11,64 @@ describe 'cassandra::schema' do
     ]
   end
 
+  let!(:stdlib_stubs) do
+    MockFunction.new('validate_hash') { |f| f.stubbed.returns(0) }
+  end
+
   let :facts do
     {
       osfamily: 'RedHat'
     }
   end
 
-  let :params do
-    {
-      cqlsh_host: 'localhost',
-      cqlsh_port: 9042
-    }
+  context 'Test that a connection test is made.' do
+    let :params do
+      {
+        cqlsh_host: 'localhost',
+        cqlsh_port: 9042
+      }
+    end
+
+    it do
+      should contain_class('cassandra::schema')
+        .with(connection_tries: 6,
+              connection_try_sleep: 30,
+              cqlsh_additional_options: '',
+              cqlsh_command: '/usr/bin/cqlsh',
+              cqlsh_host: 'localhost',
+              cqlsh_password: nil,
+              cqlsh_port: 9042,
+              cqlsh_user: 'cassandra',
+              keyspaces: [])
+    end
+
+    it do
+      read_command = '/usr/bin/cqlsh   -e \'DESC KEYSPACES\' localhost 9042'
+      should contain_exec('::cassandra::schema connection test')
+        .only_with(command: read_command,
+                   returns: 0,
+                   tries: 6,
+                   try_sleep: 30,
+                   unless: read_command)
+    end
   end
 
-  it do
-    should contain_class('cassandra::schema')
-      .with(connection_tries: 6,
-            connection_try_sleep: 30,
-            cqlsh_additional_options: '',
-            cqlsh_command: '/usr/bin/cqlsh',
-            cqlsh_host: 'localhost',
-            cqlsh_password: nil,
-            cqlsh_port: 9042,
-            cqlsh_user: 'cassandra',
-            keyspaces: [])
-  end
-
-  it do
-    should contain_exec('::cassandra::schema connection test')
-      .only_with(command: '/usr/bin/cqlsh   -e \'DESC KEYSPACES;\'',
-                 returns: 0,
-                 tries: 6,
-                 try_sleep: 30)
+  context 'Test that a keyspace can be created.' do
+    let :params do
+      {
+        cqlsh_host: 'localhost',
+        cqlsh_port: 9042,
+        keyspaces: [
+          Excelsior: {
+            ensure: 'present',
+            durable_writes: false,
+            replication_map: {
+              keyspace_class: 'SimpleStrategy',
+              replication_factor: 3
+            }
+          }
+        ]
+      }
+    end
   end
 end
