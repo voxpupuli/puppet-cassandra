e82c0e45e847525f02110540bbfc2b392c7cfb09
Ben Dalling <b.dalling@locp.co.uk>
2016-03-15 17:15:05 +0000

    Incremented the number of resources.

--- spec/classes/datastax_agent_spec.rb
+++ spec/classes/datastax_agent_spec.rb
@@ -7,7 +7,7 @@ describe 'cassandra::datastax_agent' do
   end
 
   context 'Test for cassandra::datastax_agent.' do
-    it { should have_resource_count(6) }
+    it { should have_resource_count(7) }
     it do
       should contain_class('cassandra::datastax_agent').only_with(
         'defaults_file'    => '/etc/default/datastax-agent',
