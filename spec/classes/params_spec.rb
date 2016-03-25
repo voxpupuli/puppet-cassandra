b121ff155fed4136bbdeec510e8a90a488d3bf26
Ben Dalling <b.dalling@locp.co.uk>
2016-03-25 18:47:33 +0000

    Added that things should work on Red Hat (#92).

--- spec/classes/params_spec.rb
+++ spec/classes/params_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe '::cassandra::params' do
+  let :facts do
+    {
+      osfamily: 'RedHat'
+    }
+  end
+
   it { should compile }
   it { should contain_class('cassandra::params') }
   it { should have_resource_count(0) }
