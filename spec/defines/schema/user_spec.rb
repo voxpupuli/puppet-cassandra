1b7056cb642d2d7b22234558789f4e3da2734682
Ben Dalling <b.dalling@locp.co.uk>
2016-04-25 17:53:44 +0100

    Get coverage back to 100% (#220).

---
+++ spec/defines/schema/user_spec.rb
@@ -0,0 +1,77 @@
+require 'spec_helper'
+
+describe 'cassandra::schema::user' do
+  let(:pre_condition) do
+    [
+      'define ini_setting($ensure = nil,
+         $path,
+         $section,
+         $key_val_separator       = nil,
+         $setting,
+         $value                   = nil) {}'
+    ]
+  end
+
+  context 'Create a user' do
+    let :facts do
+      {
+        osfamily: 'RedHat'
+      }
+    end
+
+    let(:title) { 'akers' }
+
+    let(:params) do
+      {
+        password: 'Niner2',
+        superuser: true
+      }
+    end
+
+    it do
+      # rubocop:disable Metrics/LineLength
+      should contain_exec('/usr/bin/cqlsh   -e "CREATE USER IF NOT EXISTS akers WITH PASSWORD \'Niner2\' SUPERUSER"  ')
+      # rubocop:enable Metrics/LineLength
+    end
+  end
+
+  context 'Drop a user' do
+    let :facts do
+      {
+        osfamily: 'RedHat'
+      }
+    end
+
+    let(:title) { 'akers' }
+
+    let(:params) do
+      {
+        password: 'Niner2',
+        ensure: 'absent'
+      }
+    end
+
+    it do
+      # rubocop:disable Metrics/LineLength
+      should contain_exec('/usr/bin/cqlsh   -e "DROP USER akers"  ')
+      # rubocop:enable Metrics/LineLength
+    end
+  end
+
+  context 'Set ensure to latest' do
+    let :facts do
+      {
+        osfamily: 'RedHat'
+      }
+    end
+
+    let(:title) { 'foobar' }
+    let(:params) do
+      {
+        ensure: 'latest'
+      }
+    end
+
+    it { should raise_error(Puppet::Error) }
+  end
+end
