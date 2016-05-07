53827c3483bb86cb48543795b66f703636bace9b
Ben Dalling <b.dalling@locp.co.uk>
2016-05-07 07:41:08 +0100

    Rubocop linting (#236).

--- scripts/travis_provision.rb
+++ scripts/travis_provision.rb
@@ -2,10 +2,8 @@ require 'rubygems'
 require 'fog'
 
 # create a connection
-ec2 = Fog::Compute.new({
-  :provider                 => 'AWS',
-  :region                   => 'eu-west-1',
-})
+ec2 = Fog::Compute.new(provider: 'AWS',
+                       region: 'eu-west-1')
 
 response = ec2.run_instances(
   'ami-b056e9c3',
@@ -16,7 +14,7 @@ response = ec2.run_instances(
   'KeyName'       => 'travis'
 )
 
-instance_id = response.body["instancesSet"].first["instanceId"]
+instance_id = response.body['instancesSet'].first['instanceId']
 instance = ec2.servers.get(instance_id)
 instance.wait_for { ready? }
-puts instance_id,':',instance.public_ip_address
+puts instance_id, ':', instance.public_ip_address
