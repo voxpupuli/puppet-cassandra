0d8dfa064d642ac1633caa853fda820699c37f9a
Ben Dalling <b.dalling@locp.co.uk>
2016-05-07 09:19:44 +0100

    Finally got all acceptance tests working as expected.  Fixes #236.

--- spec/acceptance/cassandra_ah000_schema_drop_type_spec.rb
+++ spec/acceptance/cassandra_ah000_schema_drop_type_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper_acceptance'
 
 describe 'cassandra class' do
-  schema_testing_create_pp = <<-EOS
+  schema_testing_drop_type_pp = <<-EOS
     if $::osfamily == 'RedHat' and $::operatingsystemmajrelease == 7 {
         $service_systemd = true
     } elsif $::operatingsystem == 'Debian' and $::operatingsystemmajrelease == 8 {
@@ -36,70 +36,26 @@ describe 'cassandra class' do
     $cql_types = {
       'fullname' => {
         'keyspace' => 'mykeyspace',
-        'fields'   => {
-          'fname' => 'text',
-          'lname' => 'text',
-        },
-      },
-    }
-
-    $keyspaces = {
-      'mykeyspace' => {
-        ensure          => present,
-        replication_map => {
-          keyspace_class     => 'SimpleStrategy',
-          replication_factor => 1,
-        },
-        durable_writes  => false,
-      },
+        'ensure'   => 'absent'
+      }
     }
 
     if $::operatingsystem != CentOS and $::operatingsystemmajrelease != 6 {
       class { 'cassandra::schema':
         cql_types      => $cql_types,
-        cqlsh_password => 'cassandra',
-        cqlsh_user     => 'cassandra',
-        indexes   => {
-          'users_lname_idx' => {
-             keyspace => 'mykeyspace',
-             table    => 'users',
-             keys     => 'lname',
-          },
-        },
-        keyspaces => $keyspaces,
-        tables    => {
-          'users' => {
-            'keyspace' => 'mykeyspace',
-            'columns'       => {
-              'userid'      => 'int',
-              'fname'       => 'text',
-              'lname'       => 'text',
-              'PRIMARY KEY' => '(userid)',
-            },
-          },
-        },
-        users     => {
-          'spillman' => {
-            password => 'Niner27',
-          },
-          'akers'    => {
-            password  => 'Niner2',
-            superuser => true,
-          },
-          'boone'    => {
-            password => 'Niner75',
-          },
-        },
+        cqlsh_user     => 'akers',
+        cqlsh_password => 'Niner2',
       }
     }
   EOS
 
-  describe '########### Schema create.' do
+  describe '########### Schema drop type.' do
     it 'should work with no errors' do
-      apply_manifest(schema_testing_create_pp, catch_failures: true)
+      apply_manifest(schema_testing_drop_type_pp,
+                     catch_failures: true)
     end
     it 'check code is idempotent' do
-      expect(apply_manifest(schema_testing_create_pp,
+      expect(apply_manifest(schema_testing_drop_type_pp,
                             catch_failures: true).exit_code).to be_zero
     end
   end
---
+++ spec/acceptance/cassandra_ah040_schema_drop_keyspace_spec.rb
@@ -0,0 +1,60 @@
+require 'spec_helper_acceptance'
+
+describe 'cassandra class' do
+  schema_testing_drop_pp = <<-EOS
+    if $::osfamily == 'RedHat' and $::operatingsystemmajrelease == 7 {
+        $service_systemd = true
+    } elsif $::operatingsystem == 'Debian' and $::operatingsystemmajrelease == 8 {
+        $service_systemd = true
+    } else {
+        $service_systemd = false
+    }
+
+    if $::osfamily == 'RedHat' {
+        $cassandra_optutils_package = 'cassandra22-tools'
+        $cassandra_package = 'cassandra22'
+        $version = '2.2.5-1'
+    } else {
+        $cassandra_optutils_package = 'cassandra-tools'
+        $cassandra_package = 'cassandra'
+        $version = '2.2.5'
+    }
+
+    class { 'cassandra':
+      authenticator               => 'PasswordAuthenticator',
+      cassandra_9822              => true,
+      commitlog_directory_mode    => '0770',
+      data_file_directories_mode  => '0770',
+      listen_interface            => 'lo',
+      package_ensure              => $version,
+      package_name                => $cassandra_package,
+      rpc_interface               => 'lo',
+      saved_caches_directory_mode => '0770',
+      service_systemd             => $service_systemd
+    }
+
+    $keyspaces = {
+      'mykeyspace' => {
+        ensure => absent,
+      }
+    }
+
+    if $::operatingsystem != CentOS and $::operatingsystemmajrelease != 6 {
+      class { 'cassandra::schema':
+        cqlsh_password => 'Niner2',
+        cqlsh_user     => 'akers',
+        keyspaces      => $keyspaces,
+      }
+    }
+  EOS
+
+  describe '########### Schema drop (Keyspaces).' do
+    it 'should work with no errors' do
+      apply_manifest(schema_testing_drop_pp, catch_failures: true)
+    end
+    it 'check code is idempotent' do
+      expect(apply_manifest(schema_testing_drop_pp,
+                            catch_failures: true).exit_code).to be_zero
+    end
+  end
+end
