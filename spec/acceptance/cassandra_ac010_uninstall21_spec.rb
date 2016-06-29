4183422da34c15289327e6e51dfadc4cccf1d61f
Ben Dalling <b.dalling@locp.co.uk>
2016-06-29 08:09:11 +0100

    Uninstall 2.1, not 2.0 #141.

--- spec/acceptance/cassandra_ac010_uninstall21_spec.rb
+++ spec/acceptance/cassandra_ac010_uninstall21_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper_acceptance'
 
 describe 'cassandra class' do
-  cassandra_uninstall20_pp = <<-EOS
+  cassandra_uninstall21_pp = <<-EOS
     if $::osfamily == 'RedHat' {
-        $cassandra_optutils_package = 'cassandra20-tools'
-        $cassandra_package = 'cassandra20'
+        $cassandra_optutils_package = 'cassandra21-tools'
+        $cassandra_package = 'cassandra21'
     } else {
         $cassandra_optutils_package = 'cassandra-tools'
         $cassandra_package = 'cassandra'
@@ -15,9 +15,9 @@ describe 'cassandra class' do
     }
   EOS
 
-  describe '########### Uninstall Cassandra 2.0.' do
+  describe '########### Uninstall Cassandra 2.1.' do
     it 'should work with no errors' do
-      apply_manifest(cassandra_uninstall20_pp, catch_failures: true)
+      apply_manifest(cassandra_uninstall21_pp, catch_failures: true)
     end
   end
 end
