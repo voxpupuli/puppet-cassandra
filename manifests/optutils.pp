a35a79ed8c08534afc99c8bff3d70330f81be9d6
Ben Dalling <b.dalling@locp.co.uk>
2016-06-30 09:35:10 +0100

    Removed deprecated option (ensure) #141.

--- manifests/optutils.pp
+++ manifests/optutils.pp
@@ -1,26 +1,10 @@
 # Please see the README file for the module.
 class cassandra::optutils (
-  $ensure         = 'present',
   $package_ensure = 'present',
   $package_name   = $::cassandra::params::optutils_package_name,
   ) inherits cassandra::params {
-  # Some horrific jiggerypokery until we can deprecate the ensure parameter.
-  if $ensure != present {
-    if $package_ensure != present and $ensure != $package_ensure {
-      fail('Both ensure and package_ensure attributes are set.')
-    }
-
-    cassandra::private::deprecation_warning { 'cassandra::optutils::ensure':
-      item_number => 16,
-    }
-
-    $version = $ensure
-  } else {
-    $version = $package_ensure
-  }
-
   package { $package_name:
-    ensure  => $version,
+    ensure  => $package_ensure,
     require => Class['cassandra'],
   }
 }
