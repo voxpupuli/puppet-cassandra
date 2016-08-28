f8d66955d2afbec7e6cb4caaa2a49640a1d8ca51
Ben Dalling <b.dalling@locp.co.uk>
2016-08-28 11:41:36 +0100

    First attempt at making the code D.R.Y. (#141).

--- spec/acceptance/cassandra_spec.rb
+++ spec/acceptance/cassandra_spec.rb
@@ -153,118 +153,79 @@ describe 'cassandra class' do
     end
 
     schema_testing_create_pp = <<-EOS
-    if $::osfamily == 'RedHat' {
-        $cassandra_optutils_package = 'cassandra22-tools'
-        $cassandra_package = 'cassandra22'
-        $version = '#{version}-1'
-    } else {
-        $cassandra_optutils_package = 'cassandra-tools'
-        $cassandra_package = 'cassandra'
-        $version = '#{version}'
-    }
+      #{cassandra_install_pp}
 
-    class { 'cassandra':
-      cassandra_9822              => true,
-      dc                          => 'LON',
-      package_ensure              => $version,
-      package_name                => $cassandra_package,
-      rack                        => 'R101',
-      settings                    => {
-        'authenticator'               => 'PasswordAuthenticator',
-        'cluster_name'                => 'MyCassandraCluster',
-        'commitlog_directory'         => '/var/lib/cassandra/commitlog',
-        'commitlog_sync'              => 'periodic',
-        'commitlog_sync_period_in_ms' => 10000,
-        'data_file_directories'       => ['/var/lib/cassandra/data'],
-        'endpoint_snitch'             => 'GossipingPropertyFileSnitch',
-        'listen_address'              => $::ipaddress,
-        'partitioner'                 => 'org.apache.cassandra.dht.Murmur3Partitioner',
-        'saved_caches_directory'      => '/var/lib/cassandra/saved_caches',
-        'seed_provider'               => [
-          {
-            'class_name' => 'org.apache.cassandra.locator.SimpleSeedProvider',
-            'parameters' => [
-              {
-                'seeds' => $::ipaddress,
-              },
-            ],
+      $cql_types = {
+        'fullname' => {
+          'keyspace' => 'mykeyspace',
+          'fields'   => {
+            'fname' => 'text',
+            'lname' => 'text',
           },
-        ],
-        'start_native_transport'      => true,
-      },
-    }
-
-    $cql_types = {
-      'fullname' => {
-        'keyspace' => 'mykeyspace',
-        'fields'   => {
-          'fname' => 'text',
-          'lname' => 'text',
         },
-      },
-    }
+      }
 
-    $keyspaces = {
-      'mykeyspace' => {
-        ensure          => present,
-        replication_map => {
-          keyspace_class     => 'SimpleStrategy',
-          replication_factor => 1,
+      $keyspaces = {
+        'mykeyspace' => {
+          ensure          => present,
+          replication_map => {
+            keyspace_class     => 'SimpleStrategy',
+            replication_factor => 1,
+          },
+          durable_writes  => false,
         },
-        durable_writes  => false,
-      },
-    }
+      }
 
-    if $::operatingsystem != CentOS {
-      $os_ok = true
-    } else {
-      if $::operatingsystemmajrelease != 6 {
+      if $::operatingsystem != CentOS {
         $os_ok = true
       } else {
-        $os_ok = false
+        if $::operatingsystemmajrelease != 6 {
+          $os_ok = true
+        } else {
+          $os_ok = false
+        }
       }
-    }
 
-    if $os_ok {
-      class { 'cassandra::schema':
-        cql_types      => $cql_types,
-        cqlsh_host     => $::ipaddress,
-        cqlsh_password => 'cassandra',
-        cqlsh_user     => 'cassandra',
-        indexes   => {
-          'users_lname_idx' => {
-             keyspace => 'mykeyspace',
-             table    => 'users',
-             keys     => 'lname',
-          },
-        },
-        keyspaces => $keyspaces,
-        tables    => {
-          'users' => {
-            'keyspace' => 'mykeyspace',
-            'columns'       => {
-              'userid'      => 'int',
-              'fname'       => 'text',
-              'lname'       => 'text',
-              'PRIMARY KEY' => '(userid)',
+      if $os_ok {
+        class { 'cassandra::schema':
+          cql_types      => $cql_types,
+          cqlsh_host     => $::ipaddress,
+          cqlsh_password => 'cassandra',
+          cqlsh_user     => 'cassandra',
+          indexes   => {
+            'users_lname_idx' => {
+               keyspace => 'mykeyspace',
+               table    => 'users',
+               keys     => 'lname',
             },
           },
-        },
-        users     => {
-          'spillman' => {
-            password => 'Niner27',
+          keyspaces => $keyspaces,
+          tables    => {
+            'users' => {
+              'keyspace' => 'mykeyspace',
+              'columns'       => {
+                'userid'      => 'int',
+                'fname'       => 'text',
+                'lname'       => 'text',
+                'PRIMARY KEY' => '(userid)',
+              },
+            },
           },
-          'akers'    => {
-            password  => 'Niner2',
-            superuser => true,
+          users     => {
+            'spillman' => {
+              password => 'Niner27',
+            },
+            'akers'    => {
+              password  => 'Niner2',
+              superuser => true,
+            },
+            'boone'    => {
+              password => 'Niner75',
+            },
           },
-          'boone'    => {
-            password => 'Niner75',
-          },
-        },
+        }
       }
-    }
-  EOS
+    EOS
 
     describe '########### Schema create.' do
       it 'should work with no errors' do
