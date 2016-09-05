9bf9877e64669bad8da404c214ed8972d3ed89b9
Ben Dalling <b.dalling@locp.co.uk>
2016-09-05 13:13:29 +0100

    Making a start on #271.

--- spec/classes/datastax_agent_spec.rb
+++ spec/classes/datastax_agent_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe 'cassandra::datastax_agent' do
   let(:pre_condition) do
     [
+      'class cassandra() {}',
       'define ini_setting($ensure = nil,
          $path,
          $section,
@@ -19,19 +20,35 @@ describe 'cassandra::datastax_agent' do
     end
   end
 
-  context 'Test for cassandra::datastax_agent with defaults.' do
+  context 'Test for cassandra::datastax_agent with defaults (RedHat).' do
+    let :facts do
+      {
+        osfamily: 'RedHat'
+      }
+    end
+
     it do
       should have_resource_count(4)
-      should contain_package('datastax-agent')
+
+      should contain_package('datastax-agent').with(
+        ensure: 'present'
+      )
+
+      should contain_exec('datastax_agent_reload_systemctl').with(
+        command: '/usr/bin/systemctl daemon-reload',
+        onlyif: 'test -x /usr/bin/systemctl',
+        path: ['/usr/bin', '/bin'],
+        refreshonly: true
+      ).that_notifies('Service[datastax-agent]')
+
       should contain_service('datastax-agent')
-      should contain_exec('datastax_agent_reload_systemctl')
+
       should contain_file('/var/lib/datastax-agent/conf/address.yaml')
         .with(
           owner: 'cassandra',
           group: 'cassandra'
-        )
-      should contain_file('/var/lib/datastax-agent/conf/address.yaml')
-        .that_requires('Package[datastax-agent]')
+        ).that_requires('Package[datastax-agent]')
+
       should contain_class('cassandra::datastax_agent').only_with(
         'defaults_file'        => '/etc/default/datastax-agent',
         'java_home'            => nil,
@@ -44,6 +61,23 @@ describe 'cassandra::datastax_agent' do
         'stomp_interface'      => nil,
         'local_interface'      => nil
       )
+    end
+  end
+
+  context 'Test for cassandra::datastax_agent with defaults (Debian).' do
+    let :facts do
+      {
+        osfamily: 'Debian'
+      }
+    end
+
+    it do
+      should contain_exec('datastax_agent_reload_systemctl').with(
+        command: '/bin/systemctl daemon-reload',
+        onlyif: 'test -x /bin/systemctl',
+        path: ['/usr/bin', '/bin'],
+        refreshonly: true
+      ).that_notifies('Service[datastax-agent]')
     end
   end
 
