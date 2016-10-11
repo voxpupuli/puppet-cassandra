21706bb922ba04691a478ce779a8906dccf0ecba
Joshua Knarr <Joshua.Knarr@LexisNexis.com>
2016-10-11 13:12:01 -0400

    puppet hates camelcase facts

--- lib/facter/isTmpExecutable.rb
+++ lib/facter/isTmpExecutable.rb
@@ -1,13 +1,14 @@
-Facter.add(:isTmpExecutable) do
+Facter.add(:istmpexecutable) do
   setcode do
     # if cassandra is fully installed, test for +x and ACLs on /var/tmp
-    if File.stat("/etc/cassandra/conf/jvm.options").file?
+    if File.exists? '/etc/cassandra/conf/jvm.options'
       if File.stat("/var/tmp").executable_real?
         'true'
       end
       # cassandra is installed but we can't use /var/tmp
       'false'
     end
+    # cassandra isn't installed yet
     'test is not ready yet'
   end
 end
