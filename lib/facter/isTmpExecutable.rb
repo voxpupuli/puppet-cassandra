2101cd851b07aa8bebacf8949003614b8c674922
Joshua Knarr <Joshua.Knarr@LexisNexis.com>
2016-10-11 13:29:47 -0400

    reordered for additional magic

--- lib/facter/isTmpExecutable.rb
+++ lib/facter/isTmpExecutable.rb
@@ -1,5 +1,7 @@
 Facter.add(:istmpexecutable) do
   setcode do
+    # if nothing works, assume cass isn't installed yet
+    'test is not ready yet'
     # if cassandra is fully installed, test for +x and ACLs on /var/tmp
     if File.exists? '/etc/cassandra/conf/jvm.options'
       if File.stat("/var/tmp").executable_real?
@@ -8,7 +10,5 @@ Facter.add(:istmpexecutable) do
       # cassandra is installed but we can't use /var/tmp
       'false'
     end
-    # cassandra isn't installed yet
-    'test is not ready yet'
   end
 end
