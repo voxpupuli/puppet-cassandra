d8f5e1d0313a175c5dac4f66e1a90647c2280bcd
Ben Dalling <b.dalling@locp.co.uk>
2016-11-05 08:00:45 +0000

    Rubocop appeasement (#313).

--- spec/acceptance/cassandra_spec.rb
+++ spec/acceptance/cassandra_spec.rb
@@ -438,7 +438,7 @@ describe 'cassandra' do
       end
     end
 
-    describe "########### Gather service information (when in debug mode)." do
+    describe '########### Gather service information (when in debug mode).' do
       it 'Show the cassandra system log.' do
         shell("grep -v -e '^INFO' -e '^\s*INFO' /var/log/cassandra/system.log")
       end
