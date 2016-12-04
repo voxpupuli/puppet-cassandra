2af04dadd6dbebcaa1f18c0ef0909bed8205293e
Ben Dalling <b.dalling@locp.co.uk>
2016-12-04 17:48:53 +0000

    WIP

--- rake/rake_tasks.rb
+++ rake/rake_tasks.rb
@@ -5,23 +5,22 @@ require_relative 'tasks/deploy'
 
 desc '[CI Only] Run beaker, but only for pull requests or for release branches.'
 task :acceptance do
-  Rake::Task['beaker'].invoke
-  # skip = true
-  # travis_branch = ENV['TRAVIS_BRANCH']
-  # travis_event_type = ENV['TRAVIS_EVENT_TYPE']
+  skip = true
+  travis_branch = ENV['TRAVIS_BRANCH']
+  travis_event_type = ENV['TRAVIS_EVENT_TYPE']
 
-  # if travis_event_type == 'pull_request'
-  #   skip = false
-  # elsif travis_event_type == 'push'
-  #   skip = false if travis_branch =~ /^release-/ || travis_branch =~ /^hotfix-/
-  # end
+  if travis_event_type == 'pull_request'
+    skip = false
+  elsif travis_event_type == 'push'
+    skip = false if travis_branch =~ /^release-/ || travis_branch =~ /^hotfix-/
+  end
 
-  # if skip
-  #   puts 'Skipping acceptance tests.'
-  #   exit(0)
-  # else
-  #   Rake::Task['beaker'].invoke
-  # end
+  if skip
+    puts 'Skipping acceptance tests.'
+    exit(0)
+  else
+    Rake::Task['beaker'].invoke
+  end
 end
 
 desc '[CI Only] Tag, build and push the module to PuppetForge.'
