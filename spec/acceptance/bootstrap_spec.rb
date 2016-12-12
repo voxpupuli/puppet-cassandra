80b9b5e752374bd8b35458e752cf11b0a8a9cbe4
Ben Dalling <dallinb@users.noreply.github.com>
2016-12-12 04:18:32 +0000

    Merge feature-354 into develop.

--- spec/acceptance/bootstrap_spec.rb
+++ spec/acceptance/bootstrap_spec.rb
@@ -1,21 +1,6 @@
 require 'spec_helper_acceptance'
 
 describe 'Bootstrap' do
-  whoami_pp = <<-EOS
-    notify { "operatingsystem:           ${::operatingsystem}": } ->
-    notify { "operatingsystemmajrelease: ${::operatingsystemmajrelease}": } ->
-    notify { "cassandramaxheapsize:      ${::cassandramaxheapsize}": } ->
-    notify { "cassandracmsmaxheapsize:   ${::cassandracmsmaxheapsize}": } ->
-    notify { "cassandraheapnewsize:      ${::cassandraheapnewsize}": } ->
-    notify { "cassandracmsheapnewsize:   ${::cassandracmsheapnewsize}": }
-  EOS
-
-  describe '########### Identify the node.' do
-    it 'should work with no errors' do
-      apply_manifest(whoami_pp, catch_failures: true)
-    end
-  end
-
   bootstrap_pp = <<-EOS
     case downcase($::operatingsystem) {
       'centos': {
@@ -23,8 +8,7 @@ describe 'Bootstrap' do
           exec { '/bin/cp /opt/rh/ruby200/enable /etc/profile.d/ruby.sh': } ->
           exec { '/bin/rm /usr/bin/ruby /usr/bin/gem': } ->
           exec { '/usr/sbin/alternatives --install /usr/bin/ruby ruby /opt/rh/ruby200/root/usr/bin/ruby 1000': } ->
-          exec { '/usr/sbin/alternatives --install /usr/bin/gem gem /opt/rh/ruby200/root/usr/bin/gem 1000': } ->
-          exec { '/bin/cp /opt/rh/python27/enable /etc/profile.d/python.sh': }
+          exec { '/usr/sbin/alternatives --install /usr/bin/gem gem /opt/rh/ruby200/root/usr/bin/gem 1000': }
         }
       }
       'ubuntu': {
@@ -35,25 +19,6 @@ describe 'Bootstrap' do
           package {'ruby2.0': } ->
           exec { '/bin/rm /usr/bin/ruby': } ->
           exec { '/usr/sbin/update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby2.0 1000': }
-        }
-        elsif $::operatingsystemmajrelease == 16.04 {
-          package { 'locales-all': } ->
-          package { 'net-tools': } ->
-          package { 'sudo': } ->
-          package { 'ufw': } ->
-          package { 'wget': } ->
-          package { 'ntp': } ->
-          package { 'python-pip': } ->
-          package { 'python-minimal': } ->
-          exec { '/bin/rm -f /usr/sbin/policy-rc.d': } ->
-          exec { '/usr/bin/wget http://launchpadlibrarian.net/109052632/python-support_1.0.15_all.deb':
-            cwd     => '/var/tmp',
-            creates => '/var/tmp/python-support_1.0.15_all.deb',
-          } ~>
-          exec { '/usr/bin/dpkg -i /var/tmp/python-support_1.0.15_all.deb': } ->
-          package { 'cassandra-driver':
-            provider => 'pip',
-          }
         }
       }
     }
