525fe41e25f9dcbf4422d03f5590aeb11b475190
rusty <russellshackleford35@gmail.com>
2019-03-14 17:30:04 -0400

    Add SCL wrapper for schema::cql_type

--- spec/defines/schema/cql_type_spec.rb
+++ spec/defines/schema/cql_type_spec.rb
@@ -28,7 +28,48 @@ describe 'cassandra::schema::cql_type' do
       is_expected.to compile
       is_expected.to contain_class('cassandra::schema')
       is_expected.to contain_cassandra__schema__cql_type('fullname')
-      is_expected.to contain_exec('/usr/bin/cqlsh   -e "CREATE TYPE IF NOT EXISTS Excelsior.fullname (firstname text, lastname text)" localhost 9042')
+      read_command = '/usr/bin/cqlsh   -e "DESC TYPE Excelsior.fullname" localhost 9042'
+      exec_command =  '/usr/bin/cqlsh   -e "CREATE TYPE IF NOT EXISTS Excelsior.fullname '
+      exec_command += '(firstname text, lastname text)" localhost 9042'
+      is_expected.to contain_exec(exec_command).
+        only_with(unless: read_command,
+                  require: 'Exec[::cassandra::schema connection test]')
+    end
+  end
+
+  context 'CQL TYPE (fullname) with SCL' do
+    let :facts do
+      {
+        operatingsystemmajrelease: 7,
+        osfamily: 'RedHat'
+      }
+    end
+
+    let(:title) { 'fullname' }
+
+    let(:params) do
+      {
+        'keyspace' => 'Excelsior',
+        fields:
+          {
+            'firstname' => 'text',
+            'lastname'  => 'text'
+          },
+        'use_scl'  => true,
+        'scl_name' => 'testscl'
+      }
+    end
+
+    it do
+      is_expected.to compile
+      is_expected.to contain_class('cassandra::schema')
+      is_expected.to contain_cassandra__schema__cql_type('fullname')
+      read_command = '/usr/bin/scl enable testscl "/usr/bin/cqlsh   -e \"DESC TYPE Excelsior.fullname\" localhost 9042"'
+      exec_command =  '/usr/bin/scl enable testscl "/usr/bin/cqlsh   -e \"CREATE TYPE IF NOT EXISTS Excelsior.fullname '
+      exec_command += '(firstname text, lastname text)\" localhost 9042"'
+      is_expected.to contain_exec(exec_command).
+        only_with(unless: read_command,
+                  require: 'Exec[::cassandra::schema connection test]')
     end
   end
 
@@ -53,7 +94,40 @@ describe 'cassandra::schema::cql_type' do
     it do
       is_expected.to compile
       is_expected.to contain_cassandra__schema__cql_type('address')
-      is_expected.to contain_exec('/usr/bin/cqlsh   -e "DROP type Excalibur.address" localhost 9042')
+      read_command = '/usr/bin/cqlsh   -e "DESC TYPE Excalibur.address" localhost 9042'
+      exec_command = '/usr/bin/cqlsh   -e "DROP type Excalibur.address" localhost 9042'
+      is_expected.to contain_exec(exec_command).
+        only_with(onlyif: read_command,
+                  require: 'Exec[::cassandra::schema connection test]')
+    end
+  end
+
+  context 'Set ensure to absent with SCL' do
+    let :facts do
+      {
+        operatingsystemmajrelease: 7,
+        osfamily: 'RedHat'
+      }
+    end
+
+    let(:title) { 'address' }
+    let(:params) do
+      {
+        'ensure'   => 'absent',
+        'keyspace' => 'Excalibur',
+        'use_scl'  => true,
+        'scl_name' => 'testscl'
+      }
+    end
+
+    it do
+      is_expected.to compile
+      is_expected.to contain_cassandra__schema__cql_type('address')
+      read_command = '/usr/bin/scl enable testscl "/usr/bin/cqlsh   -e \"DESC TYPE Excalibur.address\" localhost 9042"'
+      exec_command = '/usr/bin/scl enable testscl "/usr/bin/cqlsh   -e \"DROP type Excalibur.address\" localhost 9042"'
+      is_expected.to contain_exec(exec_command).
+        only_with(onlyif: read_command,
+                  require: 'Exec[::cassandra::schema connection test]')
     end
   end
 
