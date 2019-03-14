f01fa057b5513d5c78ca66445d7fbdce91c71d6e
rusty <russellshackleford35@gmail.com>
2019-03-14 17:30:04 -0400

    Add SCL wrapper for schema::table

--- spec/defines/schema/table_spec.rb
+++ spec/defines/schema/table_spec.rb
@@ -37,7 +37,61 @@ describe 'cassandra::schema::table' do
     it do
       is_expected.to compile
       is_expected.to contain_cassandra__schema__table('users')
-      is_expected.to contain_exec('/usr/bin/cqlsh   -e "CREATE TABLE IF NOT EXISTS Excelsior.users (userid text, username FROZEN<fullname>, emails set<text>, top_scores list<int>, todo map<timestamp, text>, tuple<int, text,text>, PRIMARY KEY (userid)) WITH COMPACT STORAGE AND ID=\'5a1c395e-b41f-11e5-9f22-ba0be0483c18\'" localhost 9042')
+      read_command =  '/usr/bin/cqlsh   -e "DESC TABLE Excelsior.users" localhost 9042'
+      exec_command =  '/usr/bin/cqlsh   -e "CREATE TABLE IF NOT EXISTS Excelsior.users '
+      exec_command += '(userid text, username FROZEN<fullname>, emails set<text>, top_scores list<int>, '
+      exec_command += 'todo map<timestamp, text>, tuple<int, text,text>, PRIMARY KEY (userid)) '
+      exec_command += 'WITH COMPACT STORAGE AND ID=\'5a1c395e-b41f-11e5-9f22-ba0be0483c18\'" localhost 9042'
+      is_expected.to contain_exec(exec_command).
+        only_with(unless: read_command,
+                  require: 'Exec[::cassandra::schema connection test]')
+    end
+  end
+
+  context 'Create Table with SCL' do
+    let :facts do
+      {
+        operatingsystemmajrelease: 7,
+        osfamily: 'RedHat'
+      }
+    end
+
+    let(:title) { 'users' }
+
+    let(:params) do
+      {
+        use_scl: true,
+        scl_name: 'testscl',
+        keyspace: 'Excelsior',
+        columns:
+          {
+            'userid' => 'text',
+            'username' => 'FROZEN<fullname>',
+            'emails' => 'set<text>',
+            'top_scores' => 'list<int>',
+            'todo' => 'map<timestamp, text>',
+            'COLLECTION-TYPE' => 'tuple<int, text,text>',
+            'PRIMARY KEY' => '(userid)'
+          },
+        options:
+          [
+            'COMPACT STORAGE',
+            'ID=\'5a1c395e-b41f-11e5-9f22-ba0be0483c18\''
+          ]
+      }
+    end
+
+    it do
+      is_expected.to compile
+      is_expected.to contain_cassandra__schema__table('users')
+      read_command =  '/usr/bin/scl enable testscl "/usr/bin/cqlsh   -e \"DESC TABLE Excelsior.users\" localhost 9042"'
+      exec_command =  '/usr/bin/scl enable testscl "/usr/bin/cqlsh   -e \"CREATE TABLE IF NOT EXISTS Excelsior.users '
+      exec_command += '(userid text, username FROZEN<fullname>, emails set<text>, top_scores list<int>, '
+      exec_command += 'todo map<timestamp, text>, tuple<int, text,text>, PRIMARY KEY (userid)) '
+      exec_command += 'WITH COMPACT STORAGE AND ID=\'5a1c395e-b41f-11e5-9f22-ba0be0483c18\'\" localhost 9042"'
+      is_expected.to contain_exec(exec_command).
+        only_with(unless: read_command,
+                  require: 'Exec[::cassandra::schema connection test]')
     end
   end
 
@@ -62,7 +116,40 @@ describe 'cassandra::schema::table' do
 
     it do
       is_expected.to compile
-      is_expected.to contain_exec('/usr/bin/cqlsh   -e "DROP TABLE IF EXISTS Excelsior.users" localhost 9042')
+      read_command = '/usr/bin/cqlsh   -e "DESC TABLE Excelsior.users" localhost 9042'
+      exec_command = '/usr/bin/cqlsh   -e "DROP TABLE IF EXISTS Excelsior.users" localhost 9042'
+      is_expected.to contain_exec(exec_command).
+        only_with(onlyif: read_command,
+                  require: 'Exec[::cassandra::schema connection test]')
+    end
+  end
+
+  context 'Drop Table with SCL' do
+    let :facts do
+      {
+        operatingsystemmajrelease: 7,
+        osfamily: 'RedHat'
+      }
+    end
+
+    let(:title) { 'users' }
+
+    let(:params) do
+      {
+        use_scl: true,
+        scl_name: 'testscl',
+        keyspace: 'Excelsior',
+        ensure: 'absent'
+      }
+    end
+
+    it do
+      is_expected.to compile
+      read_command = '/usr/bin/scl enable testscl "/usr/bin/cqlsh   -e \"DESC TABLE Excelsior.users\" localhost 9042"'
+      exec_command = '/usr/bin/scl enable testscl "/usr/bin/cqlsh   -e \"DROP TABLE IF EXISTS Excelsior.users\" localhost 9042"'
+      is_expected.to contain_exec(exec_command).
+        only_with(onlyif: read_command,
+                  require: 'Exec[::cassandra::schema connection test]')
     end
   end
 
