02cce7e3be4db53bc4a948c6e7d1feeecb14c431
Tim <tim@bastelfreak.de>
2021-12-16 10:15:27 +0100

    puppet-lint: fix top_scope_facts

--- examples/facts.pp
+++ examples/facts.pp
@@ -1,23 +1,23 @@
 if $facts['cassandrarelease'] {
   notify { "Cassandra release: ${facts['cassandrarelease']}": }
 } else {
-  warning('cassandrarelease is not set!')
+  warning('fact cassandrarelease is not set!')
 }
 
 if $facts['cassandramajorversion'] {
   notify { "Cassandra major version: ${facts['cassandramajorversion']}": }
 } else {
-  warning('cassandramajorversion is not set!')
+  warning('fact cassandramajorversion is not set!')
 }
 
 if $facts['cassandraminorversion'] {
   notify { "Cassandra minor version: ${facts['cassandraminorversion']}": }
 } else {
-  warning('cassandraminorversion is not set!')
+  warning('fact cassandraminorversion is not set!')
 }
 
 if $facts['cassandrapatchversion'] {
   notify { "Cassandra patch version: ${facts['cassandrapatchversion']}": }
 } else {
-  warning('cassandrapatchversion is not set!')
+  warning('fact cassandrapatchversion is not set!')
 }
